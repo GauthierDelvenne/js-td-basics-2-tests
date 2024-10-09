29953ac5086e6c1c156c9e6b3db81419a0dfd3c2
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2024-10-09 09:00:50 +0200

    Done

--- starter/script017.js
+++ starter/script017.js
@@ -10,8 +10,8 @@
 // 2. Affectez-lui la valeur 23
 // 3. Testez si la variable height est définie et affichez "La variable height est définie" ou "La variable height n'est PAS définie" selon le cas
 // 4. Testez différents cas de figure en changeant la valeur de height (par exemple 0, ou NaN) ou en ne lui affectant pas de valeur… ATTENTION, votre script doit bien dire que "La variable height est définie" dans le cas où on a donné à height une valeur de 0…
-const height = 0;
-if (height){
+const height =10;
+if (!!height){
     console.log("La variable height est définie");
 } else {
     console.log("La variable height n'est PAS définie");
@@ -23,7 +23,7 @@ if (height){
 // 2. Affectez-lui la valeur 23
 // 3. Écrivez le test qui permet d'afficher, selon le cas, "Cette variable contient bien une valeur non nulle" ou "Cette variable est nulle ou non définie"
 // 4. Testez différents cas de figure en changeant la valeur de height ou en ne lui affectant pas de valeur…
-if (height){
+if (!!height){
     console.log("Cette variable contient bien une valeur non nulle");
 } else {
     console.log("Cette variable est nulle ou non définie");
